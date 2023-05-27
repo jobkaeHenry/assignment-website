677e5666042b26b86ecf28a96b234f39eb12b6aa
Jungu Lee <1zzangjun@gmail.com>
2023-05-27 16:22:11 +0900

    :art: 등록된아이템이 없을경우 test 완료, 로직 정상화

--- src/feature/main/components/ItemsList.tsx
+++ src/feature/main/components/ItemsList.tsx
@@ -22,9 +22,9 @@ const ItemsList = ({ pageNum = 0 }: Props) => {
         최신 아이템
       </Text>
       {data?.length !== 0 ? (
-        <NotifyMessage message="등록된 아이템이 없습니다" />
-      ) : (
         data?.map((e) => <ItemCard data={e} key={e.id}></ItemCard>)
+      ) : (
+        <NotifyMessage message="등록된 아이템이 없습니다" />
       )}
     </PaddingLayout>
   );
