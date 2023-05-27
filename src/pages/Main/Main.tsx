7624aae5fc3901e302ca4158d36543898e566519
Jungu Lee <1zzangjun@gmail.com>
2023-05-27 16:20:13 +0900

    :art: fallback message 변경

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -3,7 +3,6 @@ import Skeleton from "../../components/atom/lodaing/Skeleton";
 import ItemsList from "../../feature/main/components/ItemsList";
 import MainCarousel from "../../feature/main/components/MainCarousel";
 import useSetTitle from "../../hooks/useSetTitle";
-import MobileWrapper from "../../layouts/MobileWrapper";
 import { Suspense } from "react";
 import ErrorMessage from "../../components/atom/lodaing/Error";
 
@@ -15,7 +14,7 @@ const Main = () => {
       <MainCarousel />
       <Suspense fallback={<Skeleton />}>
         <ErrorBoundary
-          fallback={<ErrorMessage message={"상품을ㄴ 불러오지 못했습니다"} />}
+          fallback={<ErrorMessage message={"상품을 불러오지 못했습니다"} />}
         >
           <ItemsList></ItemsList>
         </ErrorBoundary>
