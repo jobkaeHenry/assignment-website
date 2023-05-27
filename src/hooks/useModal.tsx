75cfb5064da39d4e250e3b949ac96a4c95c6039c
Jungu Lee <1zzangjun@gmail.com>
2023-05-27 12:45:56 +0900

    :art: Global Modal 설정

---
+++ src/hooks/useModal.tsx
@@ -0,0 +1,23 @@
+import { useRecoilState } from "recoil";
+import {
+  isModalOpenAtom,
+  modalComponentAtom,
+} from "../context/recoil/atom/globalModalAtom";
+
+/** 모달 컴포넌트를 인자로 받아 모달을 생성해주는 함수를 리턴하는 훅 */
+const useModal = () => {
+  const [ModalComponent, setModalComponent] =
+    useRecoilState(modalComponentAtom);
+  const [isModalOpen, setIsModalOpen] = useRecoilState(isModalOpenAtom);
+
+  /** 모달로 사용할 컴포넌트를 인자로 받아 모달을 생성 */
+  const onClickModal = (component: React.ReactNode) => {
+    if (ModalComponent !== component) {
+      setModalComponent(component);
+    }
+    setIsModalOpen((prev) => !prev);
+  };
+  return onClickModal;
+};
+
+export default useModal;
