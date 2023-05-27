afac8b9aa79a51039de9dff20f4d027b66a3d7c1
Jungu Lee <1zzangjun@gmail.com>
2023-05-27 21:57:01 +0900

    :art: 장바구니추가, 쿼리 초기화 기능 추가

--- src/data/URL/server/cartRoute.ts
+++ src/data/URL/server/cartRoute.ts
@@ -1,8 +1,10 @@
 /**
  * [get] 아이템 id로 상세페이지 정보를 받는 Route
  */
-export const getCartItemById = '/cart' as const
+export const getCartItemById = "/cart" as const;
 
 export const getItemRoute = (id: string) => `/items/${id}`;
 
 export const addToCartRoute = "/cart";
+
+export const deleteCartItem = (id: string) => `/cart/${id}`;
