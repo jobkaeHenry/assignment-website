f36dca1b362c643a22b3ac276fe8da9d59cb1789
Jungu Lee <1zzangjun@gmail.com>
2023-05-27 12:51:33 +0900

    :art: 로컬스토리지를 다루는 유틸

---
+++ src/utils/localStorage.tsx
@@ -0,0 +1,23 @@
+export const getLS = (key: string) => {
+  const item = localStorage.getItem(key);
+  let returnValue;
+  try {
+    if (item) returnValue = JSON.parse(item);
+    return returnValue;
+  } catch (err) {
+    return null;
+  }
+};
+
+export const setLS = (key: string, value: any) => {
+  localStorage.setItem(key, JSON.stringify(value));
+};
+
+export const removeLS = (key: string) => {
+  localStorage.removeItem(key);
+};
+
+export const cleanLS = () => {
+  localStorage.removeItem("accessToken");
+  localStorage.removeItem("refreshToken");
+};
