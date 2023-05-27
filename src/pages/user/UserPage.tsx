aded1cf1ed5c9d98696f0c698c6aa52960c68686
Jungu Lee <1zzangjun@gmail.com>
2023-05-27 22:20:35 +0900

    :sparkles: 판매중인 상품 조회기능 추가에 따른 조건부렌더링 추가

--- src/pages/user/UserPage.tsx
+++ src/pages/user/UserPage.tsx
@@ -1,5 +1,4 @@
 import { useRecoilValue } from "recoil";
-import Text from "../../components/atom/Text";
 import ErrorMessage from "../../components/atom/lodaing/Error";
 import Skeleton from "../../components/atom/lodaing/Skeleton";
 import CartItemList from "../../feature/cart/components/CartItemList";
@@ -10,21 +9,39 @@ import { ErrorBoundary } from "react-error-boundary";
 import DashBoard from "../../feature/user/components/DashBoard";
 import { RowWrapper } from "./../../layouts/Wrapper";
 import { userInfoAtom } from "../../context/recoil/atom/user";
+import SellingItemList from "../../feature/cart/components/SellingItemList";
+import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 
 const UserPage = () => {
   useSetTitle("My page");
+  useAxiosPrivate();
   const { isSellerNow } = useRecoilValue(userInfoAtom);
   return (
     <PaddingLayout>
       <RowWrapper>
         <DashBoard />
-        <Suspense fallback={<Skeleton />}>
-          <ErrorBoundary
-            fallback={<ErrorMessage message={"아이템을 불러오지 못했습니다"} />}
-          >
-            {!isSellerNow && <CartItemList />}
-          </ErrorBoundary>
-        </Suspense>
+        {!isSellerNow && (
+          <Suspense fallback={<Skeleton />}>
+            <ErrorBoundary
+              fallback={
+                <ErrorMessage message={"아이템을 불러오지 못했습니다"} />
+              }
+            >
+              <CartItemList />
+            </ErrorBoundary>
+          </Suspense>
+        )}
+        {isSellerNow && (
+          <Suspense fallback={<Skeleton />}>
+            <ErrorBoundary
+              fallback={
+                <ErrorMessage message={"아이템을 불러오지 못했습니다"} />
+              }
+            >
+              <SellingItemList />
+            </ErrorBoundary>
+          </Suspense>
+        )}
       </RowWrapper>
     </PaddingLayout>
   );
