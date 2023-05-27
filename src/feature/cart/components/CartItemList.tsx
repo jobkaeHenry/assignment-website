91384eb739a7ceef1298cb8347b4b1e43cfaf20f
Jungu Lee <1zzangjun@gmail.com>
2023-05-27 17:55:27 +0900

    :bug: Key Prop 추가

--- src/feature/cart/components/CartItemList.tsx
+++ src/feature/cart/components/CartItemList.tsx
@@ -4,19 +4,20 @@ import useAxiosPrivate from "../../../hooks/useAxiosPrivate";
 import { getCartItemById } from "../../../data/URL/server/cartRoute";
 import CartItemElem from "./CartItemElem";
 import { NotifyMessage } from "../../../components/atom/lodaing/Error";
+import { CartItemType } from "../types/cartItemsType";
 
 const CartItemList = () => {
   const axiosPrivate = useAxiosPrivate();
 
-  const { data } = useQuery<ItemType[]>(["Cartitems"], () =>
+  const { data } = useQuery<CartItemType[]>(["Cartitems"], () =>
     axiosPrivate.get(getCartItemById).then(({ data }) => data)
   );
-  console.log(data)
+  console.log(data);
   return (
     <div>
       {data?.length !== 0 ? (
         data?.map((cartItem) => (
-          <CartItemElem data={cartItem} key={cartItem.id} />
+          <CartItemElem data={cartItem} key={cartItem.itemInfo.id} />
         ))
       ) : (
         <NotifyMessage message="장바구니가 비었습니다"></NotifyMessage>
