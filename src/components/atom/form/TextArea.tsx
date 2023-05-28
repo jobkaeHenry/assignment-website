9486b03487c1b78d70c0dfb1cde6b2c4b9e6478e
Jungu Lee <1zzangjun@gmail.com>
2023-05-28 12:57:58 +0900

    :art: UI 변경

--- src/components/atom/form/TextArea.tsx
+++ src/components/atom/form/TextArea.tsx
@@ -1,18 +1,21 @@
 import styled from "@emotion/styled";
 
 type Props = {
-  height: string;
+  height?: string;
+  error?:boolean
 };
 
 const TextArea = styled.textarea`
   resize: none;
   border: 1px solid var(--line-gray);
+  
   border-radius: 8px;
   padding: 16px;
   height: ${(props: Props) => (props.height ? props.height : "")};
   &:focus{
     border: 1px solid var(--main);
   }
+  border-color:${(props: Props) => (props.error ? 'var(--alert-red)' : "")}
 `;
 
 export default TextArea;
