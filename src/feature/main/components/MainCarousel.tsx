3dd8ffc8c3f8722f95f84daa77ab9bf840c13c0b
Jungu Lee <1zzangjun@gmail.com>
2023-05-29 12:36:09 +0900

    :bug: 모바일에서 캐로샐때문에 스크롤링이 블락되는 Error Fix

--- src/feature/main/components/MainCarousel.tsx
+++ src/feature/main/components/MainCarousel.tsx
@@ -4,7 +4,7 @@ import styled from "@emotion/styled";
 
 const MainCarousel = () => {
   return (
-    <Carousel autoPlay infiniteLoop showThumbs={false} showArrows={false} emulateTouch showStatus={false}>
+    <Carousel autoPlay infiniteLoop showThumbs={false} showArrows={false} showStatus={false}>
       <CarouselWarpper src="https://d2kchovjbwl1tk.cloudfront.net/vendors/19/assets/image/1685114333408-GG_Banner_Website.jpg" />
       <CarouselWarpper src="https://d2kchovjbwl1tk.cloudfront.net/vendors/19/assets/image/1684890670432-GG_Banner_Website_8.jpg" />
     </Carousel>
@@ -20,4 +20,8 @@ const CarouselWarpper = styled.img`
   object-fit: cover;
   align-items: center;
   justify-content: center;
+  /* 모바일에서 드래그시 Swipe로 인해 화면이동이 안되는 이슈로 인해 모바일에선 사이즈를 줄임  */
+  @media screen and (max-width: 768px) {
+    height: calc(70vh - 76px);
+  }
 `;
