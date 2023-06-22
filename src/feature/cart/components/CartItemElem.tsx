d58b0dc3f665ef1e5826845318562b72c3e6fdd6
Jungu Lee <1zzangjun@gmail.com>
2023-06-22 23:07:40 +0900

    :sparkles: 수량변경 기능 추가

--- src/feature/cart/components/CartItemElem.tsx
+++ src/feature/cart/components/CartItemElem.tsx
@@ -7,10 +7,16 @@ import Text from "../../../components/atom/Text";
 import InputWithLabel from "./../../../components/atom/form/InputWithLabel";
 import { useMutation, useQueryClient } from "react-query";
 import { axiosPrivate } from "../../../lib/api/axios";
-import { deleteCartItem } from "../../../data/URL/server/cartRoute";
+import {
+  changeQuantityRoute,
+  deleteCartItem,
+} from "../../../data/URL/server/cartRoute";
 import { userInfoAtom } from "../../../context/recoil/atom/user";
 import { useRecoilValue } from "recoil";
 import fireToast from "../../../lib/toastify/fireToast";
+import { AxiosError } from "axios";
+import useDebounce from "../../../hooks/useDebounce";
+import { useEffect, useState } from "react";
 
 type Props = {
   data: CartItemType;
@@ -20,25 +26,72 @@ const CartItemElem = ({ data }: Props) => {
   const { description, _id, image, price, title } = data.itemInfo;
   const queryClient = useQueryClient();
   const { userId } = useRecoilValue(userInfoAtom);
-  const previousData = queryClient.getQueriesData([
-    "Cartitems",
-    userId,
-  ])[0][1] as CartItemType[];
-  const { mutate } = useMutation(
+  const [currentQuantity, setCurrentQuantity] = useState(data.quantity);
+  const debouncedQuantity = useDebounce(currentQuantity, 300);
+  const { mutate: changeQuantity } = useMutation(
+    ({ id, quantity }: { id: string; quantity: number }) =>
+      axiosPrivate.put(changeQuantityRoute(id), { quantity }),
+    {
+      onMutate: ({ id, quantity }) => {
+        queryClient.cancelQueries(["Cartitems", userId]);
+        const querySnapshot = queryClient.getQueryData(["Cartitems", userId]);
+        // let copyedArr: CartItemType[];
+        // if (Array.isArray(querySnapshot)) {
+        //   copyedArr = JSON.parse(JSON.stringify(querySnapshot));
+        // }
+        queryClient.setQueryData<CartItemType[]>(
+          ["Cartitems", userId],
+          (prev) => {
+            const index = (prev ?? []).findIndex((e) => e.itemInfo._id === id);
+            (prev ?? [])[index] = { ...(prev ?? [])[index], quantity };
+            return prev ?? [];
+          }
+        );
+        return { querySnapshot };
+      },
+      onError: (_err: AxiosError, _queryFnParams, context) => {
+        queryClient.setQueryData(["Cartitems", userId], context?.querySnapshot);
+        if (
+          _err.response?.status &&
+          _err.response?.status >= 400 &&
+          _err.response?.status < 500
+        ) {
+          fireToast("올바르지 않은 값입니다", "error");
+        } else fireToast("알 수 없는 오류가 발생했습니다", "error");
+      },
+      onSettled: () => {
+        queryClient.invalidateQueries(["Cartitems", userId]);
+      },
+    }
+  );
+  useEffect(() => {
+    changeQuantity({ id: _id, quantity: Number(debouncedQuantity) });
+  }, [_id, changeQuantity, debouncedQuantity]);
+
+  const { mutate: removeItem } = useMutation(
     (id: string) => axiosPrivate.delete(deleteCartItem(id)),
     {
       onMutate: () => {
-        queryClient.setQueryData(["Cartitems", userId], () =>
-          previousData.filter((e) => e.itemInfo._id !== _id)
+        queryClient.cancelQueries(["Cartitems", userId]);
+        const querySnapshot = queryClient.getQueryData(["Cartitems", userId]);
+        queryClient.setQueryData<CartItemType[]>(
+          ["Cartitems", userId],
+          (prev) => {
+            return (prev ?? []).filter((e) => e.itemInfo._id !== _id);
+          }
         );
+        return { querySnapshot };
       },
-      onError: () => {
-        queryClient.setQueryData(["Cartitems", userId], () => previousData);
-        fireToast('아이템 삭제에 실패했습니다', 'error')
+      onError: (_err, _queryFnParams, context) => {
+        queryClient.setQueryData(["Cartitems", userId], context?.querySnapshot);
+        fireToast("아이템 삭제에 실패했습니다", "error");
       },
-      onSuccess:()=>{
-        fireToast('장바구니에서 제거되었습니다','success')
-      }
+      onSuccess: () => {
+        fireToast("장바구니에서 제거되었습니다", "success");
+      },
+      onSettled: () => {
+        queryClient.invalidateQueries(["Cartitems", userId]);
+      },
     }
   );
 
@@ -60,10 +113,16 @@ const CartItemElem = ({ data }: Props) => {
         <InputWithLabel
           label="갯수"
           type="number"
-          defaultValue={data.quantity}
+          value={currentQuantity}
+          min={1}
           inputWidth="100"
+          onChange={(e) => {
+            if (Number(e.target.value) > 0) {
+              setCurrentQuantity(Number(parseInt(e.target.value)));
+            }
+          }}
         />
-        <DeleteButton onClick={() => mutate(_id)}>삭제</DeleteButton>
+        <DeleteButton onClick={() => removeItem(_id)}>삭제</DeleteButton>
       </QuantityCouterWrapper>
     </CartItemWrapper>
   );
