f8e64e9a5079c1808577f1d97b4af09c3b8aad30
Jungu Lee <1zzangjun@gmail.com>
2023-06-23 03:22:06 +0900

    :sparkles: 파일업로드 기능 추가

--- src/components/atom/form/FileInput.tsx
+++ src/components/atom/form/FileInput.tsx
@@ -1,47 +1,53 @@
 import styled from "@emotion/styled";
 import React, { InputHTMLAttributes, useId, useState } from "react";
-
+import Text from "../../atom/Text";
+import SearchIcon from '../../../assets/icons/searchIcon.svg'
 interface Props extends Omit<InputHTMLAttributes<HTMLInputElement>, "type"> {
   label: string;
 }
 
-const FileInput = (props: Props) => {
-  const { label, onChange, ...others } = props;
-  const id = useId();
+const FileInput = React.forwardRef(
+  (props: Props, ref: React.ForwardedRef<HTMLInputElement>) => {
+    const { label, onChange, ...others } = props;
+    const id = useId();
 
-  const [hasFile, setHasfile] = useState<string | ArrayBuffer | null>(null);
+    const [hasFile, setHasfile] = useState<string | ArrayBuffer | null>(null);
+    const [fileTitle, setFileTitle] = useState<string | undefined>();
 
-  const saveImgFile = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files) {
-      const file = e.target.files[0];
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
-      reader.onloadend = () => {
-        setHasfile(reader.result);
-      };
-    }
-  };
+    const saveImgFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+      if (e.target.files) {
+        const file = e.target.files[0];
+        const reader = new FileReader();
+        setFileTitle(file.name);
+        reader.readAsDataURL(file);
+        reader.onloadend = () => {
+          setHasfile(reader.result);
+        };
+      }
+    };
 
-  return (
-    <Label htmlFor={props.id ? props.id : id}>
-      {hasFile && <ImageIcon url={hasFile} />}
-      <InputTypeFile
-        onChange={(e) => {
-          saveImgFile(e);
-          onChange && onChange(e);
-        }}
-        type={"file"}
-        accept="image/*"
-        id={props.id ? props.id : id}
-        {...others}
-      />
-      {label}
-    </Label>
-  );
-};
+    return (
+      <Label htmlFor={props.id ? props.id : id}>
+        <Text typography="h4">{hasFile ? fileTitle : label}</Text>
+        {!hasFile ? <ImageIcon url={SearchIcon} /> : <ImageIcon url={hasFile} />}
+        <InputTypeFile
+          onChange={(e) => {
+            saveImgFile(e);
+            onChange && onChange(e);
+          }}
+          ref={ref}
+          type={"file"}
+          accept="image/*"
+          id={props.id ? props.id : id}
+          {...others}
+        />
+      </Label>
+    );
+  }
+);
 const ImageIcon = styled.div`
-  width: 48px;
-  height: 48px;
+  width: 200px;
+  height: 200px;
   background-size: cover;
   border-radius: 4px;
   border: 1px solid var(--line-gray);
@@ -56,6 +62,6 @@ const Label = styled.label`
   display: flex;
   align-items: center;
   gap: 8px;
-  flex-direction: row;
+  flex-direction: column;
 `;
 export default FileInput;
--- src/feature/cart/components/AddItemModal.tsx
+++ src/feature/cart/components/AddItemModal.tsx
@@ -9,54 +9,55 @@ import { createItemUrlRoute } from "../../../data/URL/server/ItemsRoute";
 import { useMutation, useQueryClient } from "react-query";
 import { userInfoAtom } from "../../../context/recoil/atom/user";
 import { useRecoilValue, useSetRecoilState } from "recoil";
-import { ItemType } from "../../items/types/itemDataTypes";
 import { isModalOpenAtom } from "../../../context/recoil/atom/globalModalAtom";
 import fireToast from "../../../lib/toastify/fireToast";
+import { getLS, setLS } from "./../../../utils/localStorage";
+import { useEffect, useState } from "react";
+import FileInput from "../../../components/atom/form/FileInput";
+import { ColumnWrapper } from "../../../layouts/Wrapper";
 
 interface NewItemFormValue {
   title: string;
   description: string;
   price: string;
-  image: string;
+  image: FileList;
 }
 
 const AddItemModal = () => {
   const {
     register,
     handleSubmit,
+    watch,
+    getValues,
     formState: { errors },
   } = useForm<NewItemFormValue>();
   const queryClient = useQueryClient();
   const { userId } = useRecoilValue(userInfoAtom);
-  const setOpenModal=  useSetRecoilState(isModalOpenAtom)
+  const setOpenModal = useSetRecoilState(isModalOpenAtom);
+  const [savedAddingItem, _] = useState(
+    getLS<NewItemFormValue>("savedAddingItem") || undefined
+  );
+  useEffect(() => {
+    const handleChange = () => {
+      const formValue = watch(["price", "description", "title"]); // 현재 값 가져오기
+      setLS("savedAddingItem", formValue); // 로컬 스토리지에 저장하기
+    };
 
-  const previousData = queryClient.getQueriesData([
-    "SellingItems",
-    userId,
-  ])[0][1] as ItemType[];
+    watch(handleChange); // 값이 변할 때마다 handleChange 함수 실행
+  }, [watch]);
 
-  const { mutate } = useMutation(
-    (data: NewItemFormValue) => {
-      return axiosPrivate.post(createItemUrlRoute, {
-        ...data,
-        image: "https://d2kchovjbwl1tk.cloudfront.net/vendor/19/product/Brown_1676186065453_resized512-jpg.webp",
-      });
+  const { mutate: submitFunction } = useMutation(
+    (data:FormData) => {
+
+      return axiosPrivate.post(createItemUrlRoute, data,{headers:{"Content-Type":'multipart/form-data'}});
     },
     {
-      onMutate: (data) => {
-        queryClient.setQueryData(["SellingItems", userId], () => [
-          ...previousData,
-          { ...data, image: "https://d2kchovjbwl1tk.cloudfront.net/vendor/19/product/Brown_1676186065453_resized512-jpg.webp", seller: userId },
-        ]);
-        setOpenModal(false)
-      },
       onError: () => {
-        fireToast('아이템 추가에 실패했습니다','error')
-        queryClient.setQueryData(["SellingItems", userId], () => previousData);
-        setOpenModal(true)
+        fireToast("아이템 추가에 실패했습니다", "error");
+        setOpenModal(true);
       },
       onSuccess: () => {
-        fireToast('상품이 추가됬습니다','success')
+        fireToast("상품이 추가됬습니다", "success");
         queryClient.invalidateQueries({
           queryKey: ["Items"],
           refetchInactive: true,
@@ -66,42 +67,83 @@ const AddItemModal = () => {
           refetchInactive: true,
         });
         queryClient.invalidateQueries({
-          queryKey: ["SellingItems",userId],
+          queryKey: ["SellingItems", userId],
           refetchInactive: true,
         });
+        setOpenModal(false);
+        setLS("savedAddingItem", null);
       },
     }
   );
 
   return (
-    <FormWrapper onSubmit={handleSubmit((data) => mutate(data))}>
-      <InputWithLabel
-        label="상품명"
-        inputWidth="100%"
+    <FormWrapper
+      onSubmit={handleSubmit((data) => {
+        const formData = new FormData();
+        formData.append("image", data.image[0]);
+        const values = Object.entries(getValues());
+        values.forEach(([key, value]) => {
+          if (key !== "image") {
+            formData.append(key, value);
+          }
+        });
+        console.log(formData)
+        submitFunction(formData);
+      })}
+    >
+      <FileInput
+        label="이미지를 추가해주세요"
         placeholder="상품명을 입력해주세요"
-        error={errors.title ? true : false}
-        {...register("title", { required: true, minLength: 3 })}
-      />
-      <InputWithLabel
-        label="가격"
-        type="number"
-        inputWidth="100%"
-        placeholder="가격을 입력해주세요"
-        unit="원"
-        error={errors.price ? true : false}
-        {...register("price", {
+        defaultValue={savedAddingItem?.title}
+        {...register("image", {
           required: true,
-          min: 1000,
-          valueAsNumber: true,
+          validate: (value) => {
+            const file = value[0];
+            if (!file) {
+              return "파일을 선택하세요.";
+            }
+            if (!file.type.includes("image")) {
+              return "이미지 파일만 허용됩니다.";
+            }
+            if (file.size > 5 * 1024 * 1024) {
+              return "파일 크기는 10MB 이하로 제한됩니다.";
+            }
+            return true;
+          },
         })}
       />
-      <Text typography="h4">상세설명</Text>
-      <TextArea
-        height={"250px"}
-        error={errors.description ? true : false}
-        {...register("description", { required: true, minLength: 10 })}
-      />
-      <Button type="submit">작성완료</Button>
+      <ColumnWrapper>
+        <InputWithLabel
+          label="상품명"
+          inputWidth="100%"
+          placeholder="상품명을 입력해주세요"
+          defaultValue={savedAddingItem?.title}
+          error={errors.title ? true : false}
+          {...register("title", { required: true, minLength: 3 })}
+        />
+        <InputWithLabel
+          label="가격"
+          type="number"
+          inputWidth="100%"
+          placeholder="가격을 입력해주세요"
+          defaultValue={savedAddingItem?.price}
+          unit="원"
+          error={errors.price ? true : false}
+          {...register("price", {
+            required: true,
+            min: 1000,
+            valueAsNumber: true,
+          })}
+        />
+        <Text typography="h4">상세설명</Text>
+        <TextArea
+          height={"250px"}
+          defaultValue={savedAddingItem?.description}
+          error={errors.description ? true : false}
+          {...register("description", { required: true, minLength: 10 })}
+        />
+        <Button type="submit">작성완료</Button>
+      </ColumnWrapper>
     </FormWrapper>
   );
 };
@@ -111,6 +153,9 @@ const FormWrapper = styled.form`
   min-width: 340px;
   flex-direction: column;
   gap: 8px;
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
+  }
 `;
 
 export default AddItemModal;
